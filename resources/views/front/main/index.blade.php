2822f6099b1effd9b60710aa49d0d54564de0493
jpjangid <developerjp2000@gmail.com>
2022-07-08 18:23:37 +0530

    Add Student portal, news&events, gallery and notices section

--- resources/views/front/main/index.blade.php
+++ resources/views/front/main/index.blade.php
@@ -138,7 +138,7 @@
             <a href="#" class="course-img" style="background-image: url(images/project-4.jpg);">
             </a>
             <div class="desc">
-              <h3><a href="#">Master of Business Administration</a></h3>
+              <h3><a href="#">MBA</a></h3>
               <p>Dignissimos asperiores vitae velit veniam totam fuga molestias accusamus alias autem provident. Odit ab aliquam dolor eius molestias accusamus alias autem provident. Odit ab aliquam dolor eius.</p>
               <span><a href="#" class="btn btn-primary btn-sm btn-course">Take A Course</a></span>
             </div>
@@ -152,9 +152,9 @@
 <section class="ftco-section ftco-no-pt ftc-no-pb">
   <div class="container">
     <div class="row d-flex">
-      <div class="col-md-5 order-md-last wrap-about wrap-about d-flex align-items-stretch">
+      <div class="col-md-5 order-md-last py-2 wrap-about wrap-about d-flex align-items-stretch">
         <div class="founder-section" style="width: 100%">
-          <div class="my-4 px-4 ">
+          <div class="px-4 ">
             <h2 class="section-heading py-3 font-weight-bold text-uppercase">
               <span class="founder">founder</span>
             </h2>
@@ -167,7 +167,7 @@
                 <p class="float-right font-weight-bold founder">- Pandit Janardan Rai Nagar</p>
               </div>
             </div>
-            <h2 class="section-heading py-3 font-weight-bold text-uppercase pt-5">
+            <h2 class="section-heading py-3 font-weight-bold text-uppercase" style="padding-top: 37px!important;">
               <span class="chancellor"> vice chancellor</span>
             </h2>
             <div class="row">
@@ -262,6 +262,174 @@
     </div>
   </div>
 </section>
+<!-- Result & Exam Form Quick links -->
+<section class="student_portal">
+    <div class="container">
+    <div class="row m-0">
+      <h2 class="news_heading2 text-uppercase mb-3 font-weight-bold">Student Portal</h2>
+    </div>
+        <div class="student_section">
+            <div class="row m-0">
+                <div class="col-lg-3 col-12 links_col">
+                  <div class="row m-0">
+                    <div class="quick_links col-lg-12 col-md-6 col-sm-6">
+                      <span class="results"><i class="fa fa-book mr-2"></i>Sem 1st Result</span>
+                    </div>
+                    <div class="quick_links col-lg-12 col-md-6 col-sm-6">
+                      <span class="results"><i class="fa fa-book mr-2"></i>Distance Examination</span>
+                    </div>
+                    <div class="quick_links col-lg-12 col-md-6 col-sm-6">
+                      <span class="results"><i class="fa fa-book mr-2"></i>Lorem, ipsum dolor</span>
+                    </div>
+                    <div class="quick_links col-lg-12 col-md-6 col-sm-6">
+                      <span class="results"><i class="fa fa-book mr-2"></i>Sem 4st Result</span>
+                    </div>
+                    <div class="quick_links col-lg-12 col-md-6 col-sm-6">
+                      <span class="results"><i class="fa fa-book mr-2"></i>13th Convocation Notice</span>
+                    </div>
+                    <div class="quick_links col-lg-12 col-md-6 col-sm-6">
+                      <span class="results"><i class="fa fa-book mr-2"></i>Sem 6st Result</span>
+                    </div>
+                    <div class="quick_links col-lg-12 col-md-6 col-sm-6">
+                      <span class="results"><i class="fa fa-book mr-2"></i>Distance Examination</span>
+                    </div>
+                    <div class="quick_links col-lg-12 col-md-6 col-sm-6">
+                      <span class="results"><i class="fa fa-book mr-2"></i>Sem 8st Result</span>
+                    </div>
+                    <div class="quick_links col-lg-12 col-md-6 col-sm-6">
+                      <span class="results"><i class="fa fa-book mr-2"></i>Lorem ipsum dolor</span>
+                    </div>
+                  </div>
+                    <!-- <ul class="quick_links my-2">
+                        <li class="my-1">Sem 1st Result</li>
+                        <li class="my-1">Sem 2nd Result</li>
+                        <li class="my-1">Sem 3rd Result</li>
+                        <li class="my-1">Sem 4th Result</li>
+                        <li class="my-1">Sem 5th Result</li>
+                        <li class="my-1">Sem 6th Result</li>
+                    </ul> -->
+                </div>
+                <div class="col-lg-9 col-12" style="padding-top:15px;">
+                    <div class="tiles_section">
+                      <div class="row m-0">
+                        <div class="col-lg-6 col-md-6 col-12 tiles_col"><img src="https://www.mlsu.ac.in/images/exam.jpg" alt=""> <div class="tiles_content">University <br> Exams</div></div>
+                      <!-- </div>
+                      <div class="row m-0"> -->
+                        <div class="col-lg-6 col-md-6 col-12 tiles_col"><img src="https://i1.wp.com/thecostaricanews.com/wp-content/uploads/2019/03/The-World-Connected-through-the-WWW.jpg?fit=1280%2C720&ssl=1" alt=""><div class="tiles_content">Digital <br> Connect</div></div>
+                      </div>
+                      <div class="row m-0">
+                        <div class="contact_person col-lg-6 col-12" style="margin:3px 0">
+                        <h5 style="color:#597cab;">GRIEVANCES</h5>
+                          <p>Please contact for any query</p>
+                        </div>
+                        <div class="contact_person col-lg-6 col-12">
+                          <div class="contact_detail row">
+                            <div class="image_row col-lg-4 col-md-4 col-12 p-0">
+                              <img src="https://www.techsparktechnologies.com/wp-content/uploads/2017/12/testimonial-dummy1.png" alt="">
+                            </div>
+                            <div class="contact_content col-lg-8 col-md-8 col-12" style="display:flex;align-items:center;">
+                              <div style="width:100%;">
+                                <h4 class="m-0" style="font-size:20px;">Jaiprakash Jangid</h4>
+                                <p class="m-0" style="font-size:15px;">jaiprakash@gmail.com</p>
+                                <p class="m-0" style="font-size:15px;">+919876887873, +917883748342</p>
+                              </div>
+                            </div>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- News & Events -->
+<section class="news_events">
+  <div class="container">
+    <div class="row m-0">
+      <div class="news_col col-lg-6 col-12">
+        <header>
+            <h2 class="news_heading text-uppercase mb-3 mt-3 font-weight-bold">News</h2>
+        </header>
+        <div class="section-content">
+            <article style="border-bottom: 1px solid lightgray; margin-bottom: 15px;">
+                <figure class="date"><i class="fa fa-file-o" style="margin-right: 5px;"></i>08-24-2014</figure>
+                <header style="margin:10px 0;"><a href="#">Distance Education Revised Fee Structure for various certificates</a></header>
+            </article><!-- /article -->
+            <article  style="border-bottom: 1px solid lightgray; margin-bottom: 15px;">
+                <figure class="date"><i class="fa fa-file-o" style="margin-right: 5px;"></i>08-24-2014</figure>
+                <header style="margin:10px 0;"><a href="#">Distance Education Revised Fee Structure for various certificates</a></header>
+            </article><!-- /article -->
+            <article>
+                <figure class="date"><i class="fa fa-file-o" style="margin-right: 5px;"></i>08-24-2014</figure>
+                <header><a href="#">Distance Education Revised Fee Structure for various certificates</a></header>
+            </article><!-- /article -->
+        </div><!-- /.section-content -->
+        <div style="padding:25px 0;font-size: 16px;">
+          <a href="" class="all_news">All News<i class="fa fa-angle-right" style="margin-left:5px;"></i></a>
+        </div>
+      </div>
+      <div class="news_col col-lg-6 col-12">
+      <section class="events small" id="events-small">
+          <header>
+              <h2 class="news_heading text-uppercase mb-2 mt-3 font-weight-bold">Events</h2>
+          </header>
+          <div class="section-content">
+              <article class="event nearest">
+                  <div class="date_image">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Emoji_u1f4c6.svg/1200px-Emoji_u1f4c6.svg.png" alt="">
+                  </div>
+                  <aside class="event-content">
+                      <header>
+                          <a href="event-detail.html">Distance Education Revised Fee Structure for various certificates.</a>
+                      </header>
+                      <div class="additional-info">Recruitments in Homeopathic Department</div>
+                  </aside>
+              </article><!-- /article -->
+              <article class="event nearest-second">
+              <div class="date_image">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Emoji_u1f4c6.svg/1200px-Emoji_u1f4c6.svg.png" alt="">
+                  </div>
+                  <aside class="event-content">
+                      <header>
+                          <a href="event-detail.html">Distance Education Revised Fee Structure for various certificates.</a>
+                      </header>
+                      <div class="additional-info clearfix">Recruitments in Homeopathic Department</div>
+                  </aside>
+              </article><!-- /article -->
+              <article class="event nearest-third">
+              <div class="date_image">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Emoji_u1f4c6.svg/1200px-Emoji_u1f4c6.svg.png" alt="">
+                  </div>
+                  <aside class="event-content">
+                      <header>
+                          <a href="event-detail.html">Distance Education Revised Fee Structure for various certificates</a>
+                      </header>
+                      <div class="additional-info">Recruitments in Homeopathic Department</div>
+                  </aside>
+              </article><!-- /article -->
+              <article class="event nearest-fourth">
+              <div class="date_image">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Emoji_u1f4c6.svg/1200px-Emoji_u1f4c6.svg.png" alt="">
+                  </div>
+                  <aside class="event-content">
+                      <header>
+                          <a href="event-detail.html">Distance Education Revised Fee Structure for various certificates</a>
+                      </header>
+                      <div class="additional-info">Recruitments in Homeopathic Department</div>
+                  </aside>
+              </article><!-- /article -->
+          </div><!-- /.section-content -->
+          <div style="padding:25px 0;font-size: 16px;">
+          <a href="" class="all_news">All Events<i class="fa fa-angle-right" style="margin-left:5px;"></i></a>
+        </div>
+      </section>
+      </div>
+    </div>
+  </div>
+</section>
+<!-- Gallery section -->
+
 <!-- about us section -->
 <section class="ftco-section ftco-counter img py-3" id="section-counter" style="background-image: url({{asset('frontend/images/college.png')}}); background-position: 50% 50%;" data-stellar-background-ratio="0.5">
   <div class="container">
@@ -283,8 +451,87 @@
     </div>
   </div>
 </section>
+<section class="gallery_section">
+  <div class="container">
+    <div class="gallery">
+      <div class="row m-0">
+        <div class="col-lg-4 col-12">
+          <h2 class="news_heading1 text-uppercase mb-3 mt-3 font-weight-bold px-0">Notice Board</h2>
+          <div class="section-content">
+            <ul style="padding-left:15px">
+              <li>
+              <article class="list_style" style="border-bottom: 1px solid lightgray; margin-bottom: 15px;">
+                  <header><a href="#">Syllabus Of Department of Psychology</a></header>
+                  <div class="additional-info1">Recruitments in Homeopathic Department</div>
+              </article><!-- /article -->
+              </li>
+              <li>
+              <article  style="border-bottom: 1px solid lightgray; margin-bottom: 15px;">
+                  <header ><a href="#">Distance Education Revised Fee Structure for various certificates</a></header>
+                  <div class="additional-info1">Recruitments in Homeopathic Department</div>
+              </article><!-- /article -->
+              </li>
+              <li>
+              <article class="list_style" style="border-bottom: 1px solid lightgray; margin-bottom: 15px;">
+                  <header><a href="#">Syllabus Of Department of Psychology</a></header>
+                  <div class="additional-info1">Recruitments in Homeopathic Department</div>
+              </article><!-- /article -->
+              </li>
+              <li>
+              <article style="border-bottom: 1px solid lightgray; margin-bottom: 15px;">
+                  <header><a href="#">Distance Education Revised Fee Structure for various certificates</a></header>
+                  <div class="additional-info1">Recruitments in Homeopathic Department</div>
+              </article><!-- /article -->
+              </li>
+              <li>
+              <article class="list_style">
+                  <header><a href="#">Syllabus Of Department of Psychology</a></header>
+                  <div class="additional-info1">Recruitments in Homeopathic Department</div>
+              </article><!-- /article -->
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-lg-8 col-12">
+        <h2 class="news_heading1 text-uppercase mb-3 mt-3 font-weight-bold">View Gallery</h2>
+          <div class="row m-0">
+            <div class="gallery_image col-lg-4 col-md-6 col-12">
+              <img src="{{ asset('frontend/images/university.jpg') }}" alt="">
+              <div class="video_icon">
+                <i class="fa fa-play-circle"></i>
+              </div>
+            </div>
+            <div class=" gallery_image col-lg-4 col-md-6 col-12">
+              <img src="{{ asset('frontend/images/college.png') }}" alt="">
+            </div>
+            <div class=" gallery_image col-lg-4 col-md-6 col-12">
+              <img src="https://jessecollege.com/wp-content/uploads/2018/03/dummy-inner.jpg" alt="" >
+            </div>
+          <!-- </div>
+          <div class="row m-0"> -->
+            <div class="gallery_image col-lg-4 col-md-6 col-12">
+              <img src="{{ asset('frontend/images/college.png') }}" alt="" >
+            </div>
+            <div class=" gallery_image col-lg-4 col-md-6 col-12">
+              <img src="https://jessecollege.com/wp-content/uploads/2018/03/dummy-inner.jpg" alt="">
+              <div class="video_icon">
+                <i class="fa fa-play-circle"></i>
+              </div>
+            </div>
+            <div class=" gallery_image col-lg-4 col-md-6 col-12">
+              <img src="{{ asset('frontend/images/university.jpg') }}" alt="" >
+            </div>
+          </div>
+          <div class="load_more">
+            <span>Load More...</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</section>
 <!-- photo gallery -->
-<section class="ftco-gallery">
+<!-- <section class="ftco-gallery">
   <div class="container-wrap">
     <div class="row justify-content-center my-4 no-gutters">
       <div class="col-md-12 text-center heading-section ftco-animate fadeInUp ftco-animated">
@@ -327,7 +574,7 @@
       </div>
     </div>
   </div>
-</section>
+</section> -->
 <!--<section class="ftco-section my-0 py-5 course-section">-->
 <!--  <div class="container-fluid px-4">-->
 <!--    <div class="row justify-content-center mb-3 pb-2">-->
@@ -382,7 +629,7 @@
 </section> -->
 
 <!-- Alumni speak -->
-<section class="testimony-section my-5 py-4">
+<!-- <section class="testimony-section my-5 py-4">
   <div class="container">
     <div class="row justify-content-center pb-2">
       <div class="col-md-8 text-center heading-section ftco-animate fadeInUp ftco-animated">
@@ -422,7 +669,7 @@
       </div>
     </div>
   </div>
-</section>
+</section> -->
 
 <!-- photo gallery -->
 <!-- <section class="ftco-gallery">
